4cb8639b00399be92a3975a71dc1e8215b22eced
Kerem Noras <kerem@noras.tech>
2024-09-28 23:01:10 +0300

    Refactor to improve typed request parameters and headers

--- index.ts
+++ index.ts
@@ -1,44 +1,52 @@
-export type TRequestParams = {
-  params?: Record<string, string>;
-};
+export type TDefinedRecords<TTypes = any> = Record<
+  keyof TTypes,
+  TTypes[keyof TTypes]
+>
 
-export type TRequestMethods = "get" | "post" | "put" | "delete";
+export type IRequestParams<TParamTypes = any> = {
+  params?: TDefinedRecords<TParamTypes>
+}
 
-export default function buildFetcher({
+export type TRequestMethods = 'get' | 'post' | 'put' | 'delete'
+
+export default function buildFetcher<THeaders>({
   baseUrl,
   headers,
 }: {
-  baseUrl?: string;
-  headers?: Record<string, string> | (() => Record<string, string>);
+  baseUrl?: string
+  headers?:
+    | Headers
+    | TDefinedRecords<THeaders>
+    | (() => TDefinedRecords<THeaders>)
 }) {
   return new Proxy(
     {} as {
       [key in TRequestMethods]: <T = any>(
         path: string,
-        options?: RequestInit & TRequestParams
-      ) => Promise<T>;
+        options?: RequestInit & IRequestParams
+      ) => Promise<T>
     },
     {
       get:
         (_, method) =>
-        async (path: string, options: RequestInit & TRequestParams = {}) => {
-          const url = new URL(`${baseUrl}${path}`);
+        async (path: string, options: RequestInit & IRequestParams = {}) => {
+          const url = new URL(`${baseUrl}${path}`)
           if (options.params) {
             Object.entries(options.params).forEach(([key, value]) => {
-              url.searchParams.append(key, value);
-            });
+              url.searchParams.append(key, value)
+            })
           }
 
           const response = await fetch(url, {
             headers: {
-              ...(typeof headers === "function" ? headers() : headers),
+              ...(typeof headers === 'function' ? headers() : headers),
               ...options.headers,
             },
             ...options,
             method: method as string,
-          });
-          return response.json();
+          })
+          return response.json()
         },
     }
-  );
+  )
 }
